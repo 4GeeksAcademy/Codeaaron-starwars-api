ec60156c7f2102080490048998f69fa47ceeb0ef
CodeAaronM <aaron.martinez.b@gmail.com>
2024-09-13 19:13:40 +0000

    se agrego la funcioon de get user

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,20 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    all_users = User.query.all()
+    users = list(map(lambda user: user.serialize(),all_users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(users), 200
+ 
+@app.route('/character', methods=['GET'])
+def get_character():
+    all_characters = Character.query.all()
+    characters = list(map(lambda character: character.serialize(),all_characters))
+    
+    return jsonify(characters), 200
+ 
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -17,3 +18,85 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Character(db.Model):
+    __tablename__ = 'Character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+    height = db.Column(db.String(250))
+    skin_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+
+    
+    def __repr__(self):
+        return '<Character %r>' % self.Character
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color
+        }
+
+# class Ship(db.Model):
+#     __tablename__ = 'Ship'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250), nullable=False)
+#     climate = db.Column(db.String(250))
+#     population = db.Column(db.String(250))
+#     orbital_period = db.Column(db.String(250))
+#     rotation_period = db.Column(db.String(250))
+#     diameter = db.Column(db.String(250))
+
+# class Planet(db.Model):
+#     __tablename__ = 'Planet'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250))
+#     Model = db.Column(db.String(250))
+#     manufacturer = db.Column(db.String(250))
+#     cost_in_credits = db.Column(db.Integer)
+#     crew = db.Column(db.Integer)
+
+# class User(db.Model):
+#     __tablename__ = 'User'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250), nullable=False)
+
+# class User_character(db.Model):
+#     __tablename__ = 'User_character'
+#     id = db.Column(db.Integer, primary_key=True)
+#     User_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+#     User = db.relationship(User)
+#     Character_id = db.Column(db.Integer, db.ForeignKey('Character.id'))
+#     Character = db.relationship(Character)
+
+# class User_Planet(db.Model):
+#     __tablename__ = 'User_Planet'
+#     id = db.Column(db.Integer, primary_key=True)
+#     User_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+#     User = db.relationship(User)
+#     Planet_id = db.Column(db.Integer, db.ForeignKey('Planet.id'))
+#     Planet = db.relationship(Planet)
+
+# class User_Ship(db.Model):
+#     __tablename__ = 'User_Ship'
+#     id = db.Column(db.Integer, primary_key=True)
+#     User_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+#     User = db.relationship(User)
+#     Ship_id = db.Column(db.Integer, db.ForeignKey('Ship.id'))
+#     Ship = db.relationship(Ship)
+
+# class Character_Ship(db.Model):
+#     __tablename__ = 'Character_Ship'
+#     id = db.Column(db.Integer, primary_key=True)
+#     Character_id = db.Column(db.Integer, db.ForeignKey('Character.id'))
+#     Character = db.relationship(Character)
+#     Ship_id = db.Column(db.Integer, db.ForeignKey('Ship.id'))
+#     Ship = db.relationship(Ship)
