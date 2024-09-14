887afa05101a15ad3010637057a1bdaad7ad92bd
CodeAaronM <aaron.martinez.b@gmail.com>
2024-09-14 07:59:10 +0000

    se creo el post delete

--- src/app.py
+++ src/app.py
@@ -47,10 +47,54 @@ def get_user():
 def get_character():
     all_characters = Character.query.all()
     characters = list(map(lambda character: character.serialize(),all_characters))
-    
     return jsonify(characters), 200
- 
 
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character_by_id(character_id):
+    character = Character.query.filter_by(id=character_id).first()
+
+    if character is None:
+        return jsonify({"error": "Character not found"}), 404
+
+    return jsonify(character.serialize()), 200
+
+@app.route('/character', methods=['POST'])
+def post_character():
+    body = request.get_json()
+
+    if 'name' not in body:
+        return jsonify('debes poner un nombre'), 200
+    if 'birth_year' not in body:
+        return jsonify('debes poner un birth_year'), 200
+    if 'gender' not in body:
+        return jsonify('debes poner un gender'), 200
+    if 'height' not in body:
+        return jsonify('debes poner un height'), 200
+    if 'skin_color' not in body:
+        return jsonify('debes poner un skin_color'), 200
+    if 'eye_color' not in body:
+        return jsonify('debes poner un eye_color'), 200
+    
+    if body['name'] == '':
+        return jsonify('el nombre no puede estar vacio'), 200
+    
+    character = Character(name=body['name'], birth_year=body['birth_year'], gender=body['gender'], height=body['height'], skin_color=body['skin_color'],eye_color=body['eye_color'])
+    db.session.add(character)
+    db.session.commit()
+    return jsonify('se creo character exitosamente'), 200
+
+
+@app.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character_by_id(character_id):
+    character = Character.query.filter_by(id=character_id).first()
+
+    if character is None:
+        return jsonify({"error": "Character not found"}), 404
+    
+    db.session.delete(character)
+    db.session.commit()
+
+    return jsonify(character.serialize()), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
